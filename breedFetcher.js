eb074667b4b594c69f8ccf9cabcb8487693cee00
Hisban Shiraz <hisbanbardai@gmail.com>
2023-09-24 17:24:01 -0600

    Created index.js file and moved the invoking part of fetchBreedDescription to it

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,20 @@
 const request = require("request");
 const API_ENDPOINT = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-//Saving command line arguments in a variable
-const args = process.argv.slice(2);
+const fetchBreedDescription = function (breedName, callback) {
+  request(API_ENDPOINT + breedName, (error, response, body) => {
+    //check if any error with request
+    if (error) {
+      callback(error, null);
+    } else {
+      //parsing body from string into an object
+      const data = JSON.parse(body);
+      //check if breed exists
+      data[0]
+        ? callback(null, data[0].description)
+        : callback(error, null);
+    }
+  });
+};
 
-request(API_ENDPOINT + args[0], (error, response, body) => {
-  //check if any error with request
-  if (error) {
-    console.error(error);
-  } else {
-    //parsing body from string into an object
-    const data = JSON.parse(body);
-    //check if breed exists
-    data[0]
-      ? console.log(data[0].description)
-      : console.log("Breed not found.");
-  }
-});
+module.exports = fetchBreedDescription;
